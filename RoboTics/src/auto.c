e7f56c30a0a4e4667f83714fa66796b2951a4c96
vmusser <vmusser@gmail.com>
2019-03-22 19:56:03 -0500

    Update auto.c

--- RoboTics/src/auto.c
+++ RoboTics/src/auto.c
@@ -1,61 +1,94 @@
-#include "motor.h"
-#include "API.h"
 #include "main.h"
 
-double inchToTick(double inches) {
-  double inchesPerRotation = 4.25 * 3.14159265;
-  double ticks = (inches/inchesPerRotation) * 360;
-  return ticks;
-}
 
-/*
-double PID(int requestedVal, Encoder encoder, double kp, double ki, double kd){
-  double integral = 0;
-  double derivative = 0;
-  double previousErr = 0;
-  double power = 0;
-  double ticks = 0;
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+#include "okapi/api.hpp"
+using namespace okapi;
 
- while(ticks <= requestedVal){
-          double error = requestedVal - ticks;
-          integral += error/50;
-          derivative = (error - previousErr)*50;
-          previousErr = error;
-          power = error*kp + integral*ki + derivative*kd;
-          delay(20);
-          ticks = encoderGet(encoder);
-      }
-      if(integral >= 10000)
-        integral = 4000; //These are soft maximums for testing
-      if(ticks >= requestedVal){
-          integral = 0;
-          power = 0;
-        }
-          return power;
+okapi:: MotorGroup right({11, 20});
+okapi:: MotorGroup left({1, 10});
+
+const auto WHEEL_DIAMETER = 4.25_in;
+const auto CHASSIS_WIDTH = 13.5_in;
+
+pros::Motor launcher(4,5);
+pros::Motor intake(8);
+
+auto chassis = ChassisControllerFactory::create(
+  left, right,
+  AbstractMotor::gearset::green,
+  {WHEEL_DIAMETER, CHASSIS_WIDTH}
+);
+
+/* intake
+void intake(double msec){
+intake.moveVelocity(200);
+delay(msec);
+intake.moveVelocity(0);
 }
 */
 
-double Proportion(int requestedVal, Encoder encoder, double kp){
-  double integral = 0;
-  double derivative = 0;
-  double previousErr = 0;
-  double power = 0;
-  double ticks = 0;
-  double error = requestedVal - ticks;
-  power = error*kp;
-  if(power > 80)
-    power = 80;
-  return power;
-}
+/*
+float speedChangePerLoop = 0.06;
 
-void Adrive (int ltocks, int rtocks, Encoder lEncoder, Encoder rEncoder) {
-  motorSet(leftBack, Proportion(inchToTick(ltocks), lEncoder, .25));
-  motorSet(leftFront, Proportion(inchToTick(ltocks), lEncoder, .25));
-  motorSet(rightBack, Proportion(inchToTick(ltocks), lEncoder, .25));
-  motorSet(rightFront, Proportion(inchToTick(ltocks), lEncoder, .25));
-}
+float leftPower = 0, rightPower = 0;
 
-void Alaunch() {
-		motorSet(launcher, -127);
-    delay(1000);
+void accelerateDrive(int leftTargetPower, int rightTargetPower)
+{
+	if(leftTargetPower < leftPower)
+	{
+		leftPower -= speedChangePerLoop;
+    chassis.setMaxVelocity(leftPower);
+	}
+	else if(leftTargetPower > leftPower)
+	{
+		leftPower += speedChangePerLoop;
+    chassis.setMaxVelocity(leftPower);
+	}
+	else{}
+
+	if(rightTargetPower < rightPower)
+	{
+		rightPower -= speedChangePerLoop;
+    chassis.setMaxVelocity(rightPower);
+	}
+	else if(rightTargetPower > rightPower)
+	{
+		rightPower += speedChangePerLoop;
+    chassis.setMaxVelocity(rightPower);
+	}
+	else{}
+
+}
+*/
+
+//auto PID = IterativePosPIDController::create(double ikP, double ikI, double ikD);
+
+void autonomous() {
+  right.setReversed(true);
+  //accelerateDrive(100, 100);
+  //chassis.moveDistance(1_ft);
+
+  chassis.setMaxVelocity(125);
+  chassis.moveDistance(1_ft); //moves in position to shoot
+  intake = -75;
+  pros::delay(500);
+  chassis.moveDistance(2.6_ft); //moves forward to hit the bottom flag
+  chassis.moveDistance(-3.6_ft); //moves backward to the starting position
+  chassis.turnAngle(-90_deg); //turns toward the cap
+  intake = 100;
+  pros::delay(1000);
+  chassis.moveDistance(2.6_ft); //move forward to take the ball
+
+
 }
